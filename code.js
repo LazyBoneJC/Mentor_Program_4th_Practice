18545b69753e68b4bf8405b3eeb65079f1ab6514
Walter Chang <jackyhaha8@gmail.com>
2022-06-27 16:48:12 +0800

    js object

--- code.js
+++ code.js
@@ -1,6 +1,11 @@
-hello();
-
-function hello() {
-  console.log("Hey, I am on GitHub!");
-  console.log("This is a new branch.");
+var student = [];
+var walter = {
+  name: "walter",
+  dep: "im",
+  tel: "0919922708",
+  loc: "taiwan taoyuan"
 };
+
+student.push(walter);
+
+console.log(walter.name);
